a35e6af3b3e4a0f18db3d1f47313513070aec7af
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-05-23 01:12:09 +0400

    Add project files.

---
+++ Homework(Karma)/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Homework_Karma_.Data_Access_Layer;
+using Homework_Karma_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_Karma_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
---
+++ Homework(Karma)/Data Access Layer/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Homework_Karma_.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_Karma_.Data_Access_Layer
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+             
+        }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
